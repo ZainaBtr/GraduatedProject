0d1a1ec16c0ad6af76e375deb92b9b80f5d6af2b
neamaaltahan73 <neamaaltahan2000@gmail.com>
2024-05-28 14:00:22 +0300

    dynamic dropdawn yearandsplizaition

--- resources/views/layouts/main-sidebar.blade.php
+++ resources/views/layouts/main-sidebar.blade.php
@@ -13,70 +13,7 @@
 </li>
 
                     <!-- Years & Specializations-->
-                    <li>
-                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#Years&Specializations">
-                            <div class="pull-left"><i class="ti-calendar"></i><span
-                                    class="right-nav-text">Years & Specializations</span></div>
-                            <div class="pull-right"><i class="ti-plus"></i></div>
-                            <div class="clearfix"></div>
-                        </a>
-                        <ul id="Years&Specializations" class="collapse" data-parent="#sidebarnav">
-                            <li><a href="{{route('showAllServicesYearsAndSpecializations')}}">listYears&Specializations</a></li>
-
-                        </ul>
-                    </li>
-                     <!-- Roles-->
-                    <li>
-                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#Roles">
-                            <div class="pull-left"><i class="ti-pencil-alt"></i><span
-                                    class="right-nav-text">Roles</span></div>
-                            <div class="pull-right"><i class="ti-plus"></i></div>
-                            <div class="clearfix"></div>
-                        </a>
-                        <ul id="Roles" class="collapse" data-parent="#sidebarnav">
-                            <li><a href="{{route('showAllRoles')}}">listRoles</a></li>
-
-                        </ul>
-                    </li>
-                     <!-- Advanced users-->
-                    <li>
-                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#Advanced users">
-                            <div class="pull-left"><i class="ti-user"></i><span
-                                    class="right-nav-text">AdvancedUsers</span></div>
-                            <div class="pull-right"><i class="ti-plus"></i></div>
-                            <div class="clearfix"></div>
-                        </a>
-                        <ul id="Advanced users" class="collapse" data-parent="#sidebarnav">
-                            <li><a href="{{route('showAllAdvancedUsers')}}">list Advanced userss</a></li>
-
-                        </ul>
-                    </li>
-                     <!-- Normal users-->
-                    <li>
-                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#Normal users">
-                            <div class="pull-left"><i class="ti-user"></i><span
-                                    class="right-nav-text">NormalUsers</span></div>
-                            <div class="pull-right"><i class="ti-plus"></i></div>
-                            <div class="clearfix"></div>
-                        </a>
-                        <ul id="Normal users" class="collapse" data-parent="#sidebarnav">
-                            <li><a href="">listNormalusers</a></li>
-
-                        </ul>
-                    </li>
-                     <!-- PublicServices-->
-                     <li>
-                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#Publicservices">
-                            <div class="pull-left"><i class="ti-support"></i><span
-                                    class="right-nav-text">PublicServices</span></div>
-                            <div class="pull-right"><i class="ti-plus"></i></div>
-                            <div class="clearfix"></div>
-                        </a>
-                        <ul id="Publicservices" class="collapse" data-parent="#sidebarnav">
-                            <li><a href="">listPublicServices</a></li>
-
-                        </ul>
-                    </li>
+                  
                      <!-- FavoriteServices-->
                      <li>
                         <a href="javascript:void(0);" data-toggle="collapse" data-target="#Favoriteservices">
